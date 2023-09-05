791f3c56dac6c186b3ef968868272656beb4f94d
bergaman9 <omerguler53@gmail.com>
2023-09-05 13:22:39 +0300

    captcha protection update

--- cogs/owo.py
+++ cogs/owo.py
@@ -15,6 +15,7 @@ class OwO(commands.Cog):
         self.won = None
         self.amount = 100
         self.earned = 0
+        self.active = True
         self.channel_id = os.getenv("CHANNEL_ID")
         self.owo_loop.start()
 
@@ -29,26 +30,35 @@ class OwO(commands.Cog):
         }
 
     @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.id == 408785106942164992:
+            if "complete your captcha to verify that you are human!" in message.content.lower():
+                self.active = False
+
+    @commands.Cog.listener()
     async def on_message_edit(self, before, after):
         if after.author.id == 408785106942164992:  # owo botunun Discord ID'si
             if "and you won" in after.content.lower():  # Kazandıysak
                 self.won = True
                 self.amount += 50
-                self.earned += self.amount * 2  # amount'un 2 katını ekleyin
+                self.earned += self.amount
             elif "and you lost it all..." in after.content.lower():  # Kaybettiysek
                 self.won = False
                 if self.amount > 50:
                     self.amount -= 50
-                self.earned -= self.amount  # amount'un 2 katını çıkarın
+                self.earned -= self.amount
 
     @tasks.loop(seconds=1)
     async def owo_loop(self):
+        if not self.active:  # Eğer self.active False ise, loop'u durdur
+            return
+
         await self.bot.wait_until_ready()
         print(f"Kazanma Durumu: {self.won}, Toplam Kazanç: {self.earned}")  # Toplam kazanç veya kaybı yazdır
         payload = self.create_payload()
         requests.post(f"https://discord.com/api/v9/channels/{self.channel_id}/messages", headers=self.headers, json=payload)
 
-        random_seconds = randint(20, 40)  # Örneğin, 20 ile 40 saniye arasında rastgele bir zaman seç
+        random_seconds = randint(15, 25)  # Örneğin, 20 ile 40 saniye arasında rastgele bir zaman seç
         print(f"{random_seconds} saniye sonra yeni komut çalıştırılacak.")
         await sleep(random_seconds)  # Seçilen süre kadar bekler
 
